b597c2174602419c9ef467d1503c072e6b2a19bb
Seidgy Amano <seidgy@gmail.com>
2023-05-08 13:04:20 -0300

    Meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,20 +3,23 @@ import { resolve } from "path";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  meta: {
-    title: "Boilerplate DS",
-    meta: [
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-    ],
-    script: [],
-    link: [
-      { rel: "stylesheet", href: "https://use.typekit.net/tsd2tcy.css" },
-      {
-        rel: "stylesheet",
-        href:
-          "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
-      },
-    ],
+  app: {
+    head: {
+      title: "Glam-e Lab logo generator",
+      meta: [
+        { name: "descrtiption", content: "A tool to generate the logo files and branding information for Glam-e Lab" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      script: [],
+      link: [
+        { rel: "stylesheet", href: "https://use.typekit.net/tsd2tcy.css" },
+        {
+          rel: "stylesheet",
+          href:
+            "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
+        },
+      ],
+    }
   },
   css: [
     '@ccmdesign/ccm-ds/assets/css/main.scss',
